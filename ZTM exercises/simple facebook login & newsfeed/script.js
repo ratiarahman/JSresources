4ba25f6d4001ec64971e4d021f7530c1bff52016
ratiarahman <ratia_rahman@yahoo.com>
2019-07-03 15:41:41 +0600

    well now we multiple users can log in

--- ZTM exercises/simple facebook login & newsfeed/script.js
+++ ZTM exercises/simple facebook login & newsfeed/script.js
@@ -2,6 +2,22 @@ var database = [
 	{
 		username: "ratiarahman",
 		password: "secretpass"
+	},
+	{
+		username: "fatima",
+		password: "12345"
+	},
+	{
+		username: "vanya",
+		password: "hellyeah"
+	},
+	{
+		username: "jssocool",
+		password: "fire"
+	},
+	{
+		username: "vala",
+		password: "lalaland"
 	}
 ];
 
@@ -25,8 +41,17 @@ var newsfeed = [
 var userName = prompt("Username");
 var passWord = prompt("Password");
 
+function isUserValid(u,p){
+	for(var i=0; i<database.length; i++){
+		if (u===database[i].username && p===database[i].password) {
+			return true;
+		}
+	}
+	return false;
+}
+
 function signIn(user, pass){
-	if (user === database[0].username && pass === database[0].password) {
+	if (isUserValid(user,pass)) {
 		// document.getElementById("login").innerHTML = newsfeed;
 		console.log(newsfeed);
 	}
