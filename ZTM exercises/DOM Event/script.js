acdf85578f38e1ea69eae18ec5ed13738776bc4e
ratiarahman <ratia_rahman@yahoo.com>
2019-07-05 13:37:38 +0600

    DRY: do not repeat yourself also worked

--- ZTM exercises/DOM Event/script.js
+++ ZTM exercises/DOM Event/script.js
@@ -7,22 +7,32 @@ var button = document.getElementsByTagName("button")[1];
 var ul = document.querySelector("ul");
 var input = document.getElementById("inputItem")
 
-button.addEventListener("click", function(){                //mouse event 
-	// console.log("Yeah! It works");
-	if (input.value.length > 0) {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
-	}
-})
+//DRY:DON'T REPEAT YOURSELF
+function inputLength(){
+	return input.value.length;
+}
 
-input.addEventListener("keypress", function(event){                //keypress event 
-	// console.log(event.which);
-	if (input.value.length > 0 && event.which===13) {
+function createListElement(){
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	input.value = "";
+}
+
+function clickEvent(){                          //mouse event 
+	// console.log("Yeah! It works");
+	if (inputLength() > 0) {
+	createListElement();
 	}
-})
+}
+
+function KeyEvent(event){                       //keypress event //event.keyCode also okay
+	// console.log(event.which);
+	if (inputLength() > 0 && event.which===13) {
+	createListElement();
+	}
+}
+
+button.addEventListener("click", clickEvent)
+
+input.addEventListener("keypress", KeyEvent
