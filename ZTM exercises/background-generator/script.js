e74eaeb0743a8452f89b6eb7ea29011feb036f16
ratiarahman <ratia_rahman@yahoo.com>
2019-07-23 10:12:54 +0600

    two random hex color get

--- ZTM exercises/background-generator/script.js
+++ ZTM exercises/background-generator/script.js
@@ -3,18 +3,26 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.querySelector("body");
 
+
+function random(){
+	
+	var x = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+	var y = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+  	//return x;
+  	document.getElementById("demo").value = x;
+  	document.getElementById("one").value = y;
+}
+
 function backgroundColor(){
 	var x = body.style.background = "linear-gradient(to right," + color1.value + "," + color2.value + ")";
 	h3.textContent = x;
-	// console.log(x);
+	// console.log(x);	
 }
 
+backgroundColor();
 // color1.addEventListener("input", backgroundColor());
 
 // color2.addEventListener("input", backgroundColor());
-backgroundColor();
-
-// color1.addEventListener("input", backgroundColor);
-
+color1.addEventListener("input", backgroundColor);
 color2.addEventListener("input", backgroundColor);
 
